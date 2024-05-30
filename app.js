cdd3820dd315d30090e07a76d920a8e4be0dd0d7
John <john@test.com>
2024-05-30 15:23:50 +0800

    大致上完成，後續要做細節調整

--- app.js
+++ app.js
@@ -12,6 +12,42 @@ let ySpeed = 20;
 let ground_x = 100;
 let ground_y = 500;
 let ground_height = 5;
+let brickArray = [];
+
+// 取得介於某兩數字之間的值
+function getRandomArbitrary(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+// 製作磚塊
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 50;
+    this.height = 50;
+    brickArray.push(this);
+  }
+
+  drawBrick() {
+    ctx.fillStyle = "lightgreen";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  touchingBall(ballX, ballY) {
+    return (
+      ballX >= this.x - radius &&
+      ballX <= this.x + this.width + radius &&
+      ballY >= this.y - radius &&
+      ballY <= this.y + this.height + radius
+    );
+  }
+}
+
+// 製作所有的brick
+for (let i = 0; i < 10; i++) {
+  new Brick(getRandomArbitrary(0, 950), getRandomArbitrary(0, 550));
+}
 
 // 追蹤滑鼠位置，但只在canvas裡面追蹤，而非整個window
 c.addEventListener("mousemove", (e) => {
@@ -23,6 +59,39 @@ c.addEventListener("mousemove", (e) => {
 });
 
 function drawCircle() {
+  // TODO -------------------------確認球是否打到磚塊-------------------------
+  brickArray.forEach((v, i) => {
+    if (v.touchingBall(circle_x, circle_y)) {
+      // 改變x, y方向速度，並且將brick從brickArray中移除
+      // 判斷球是從哪個方向撞到brick
+      // 從下方撞擊
+      if (circle_y >= v.y + v.height) {
+        ySpeed *= -1;
+      }
+      // 從上方撞擊
+      else if (circle_y <= v.y) {
+        ySpeed *= -1;
+      }
+      // 從左方撞擊
+      else if (circle_x <= v.x) {
+        xSpeed *= -1;
+      }
+      // 從右方撞擊
+      else if (circle_x >= v.x + v.width) {
+        xSpeed *= -1;
+      }
+      // 刪除掉撞擊到的brick
+      brickArray.splice(i, 1);
+      // 遊戲結束判定
+      if (brickArray.length == 0) {
+        alert("遊戲結束");
+        clearInterval(game);
+      }
+    }
+  });
+
+  // TODO -------------------------確認球是否打到磚塊-------------------------
+
   // -------------------------確認球是否打到橘色地板-------------------------
   // 球的x介於地板之間才反彈
   if (
@@ -69,6 +138,11 @@ function drawCircle() {
   // 畫出黑色背景
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+  // 畫出所有的brick
+  brickArray.forEach((v) => {
+    v.drawBrick();
+  });
 
   // 畫出可控制的地板
   ctx.fillStyle = "orange";
